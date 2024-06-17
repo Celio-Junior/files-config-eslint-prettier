f0444e6c73081a696e555808c8e44197b24b9cfc
Celio junior <143201092+Celio-Junior@users.noreply.github.com>
2024-06-17 20:14:28 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@babel/eslint-parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
     },
-    babelOptions: {
-      presets: ["@babel/preset-react"],
-    },
   },
-  plugins: ["react", "prettier", "react-hooks"],
+  plugins: ['react', 'react-hooks'],
   rules: {
-    "prettier/prettier": 2,
-    "react/jsx-filename-extension": 0,
-    "import/prefer-default-export": 0,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
